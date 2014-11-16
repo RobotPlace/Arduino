1b0b155cccbbf02778f43886054e15d30083be46
robotmonitor <admin@robotmonitor.com>
2014-11-16 22:04:55 +0100

    Create Sensor_Mesh_server_No_Sensor_ver1.1.ino

---
+++ Examplex/Sensor_Mesh_server_No_Sensor/Sensor_Mesh_server_No_Sensor_ver1.1.ino
@@ -0,0 +1,12 @@
+/*
+Following data need be set:
+Device Auth Key
+DataTable in key
+
+More optimised version
+
+1) server has portTCP predefined :: ex. 50160
+2) server gets its TCP IP address from DHCP
+3) ping online status to the portal.robotmonitor.com
+4) send data to DataTable every 1 minute
+*/
