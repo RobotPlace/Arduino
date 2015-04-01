1bcb0d4c84215092e698d0475994813debc8d782
robotmonitor <admin@robotmonitor.com>
2015-04-01 11:37:53 +0200

    Update LED_4x_http_analog.ino

--- Examples/LED_4x_http_analog/LED_4x_http_analog.ino
+++ Examples/LED_4x_http_analog/LED_4x_http_analog.ino
@@ -17,7 +17,18 @@
 	e = LED 3 ON
 	a = LED 4 ON
 	s = All LEDs OFF
-
+	
+ RobotMonitor HTTP CALLS
+	send command example:
+		LAN: http://192.168.1.180:50180/a
+		WAN: http://yourDomain:50180/a
+	send pingonline status example:
+		http://portal.robotmonitor.com/device-acces/write/?hash=yourDeviceAuthHashKey&data=pingonline
+	save DataTable example:
+		http://portal.robotmonitor.com/device-acces/write/?hash=yourDataTableINLink&data={"example":123}
+	read DataTable example:
+		http://portal.robotmonitor.com/device-acces/write/?hash=yourDataTableOUTLink
+	
  Circuit:
   - Arduino
   - Arduino Ethernet shield
