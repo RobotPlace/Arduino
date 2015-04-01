b87e5a272d3d3e81acbfa5f97d946c1156abb4dc
robotmonitor <admin@robotmonitor.com>
2015-04-01 11:43:05 +0200

    Update LED_HTTP_analog_ping.ino

--- Examples/LED_http_analog_ping/LED_HTTP_analog_ping.ino
+++ Examples/LED_http_analog_ping/LED_HTTP_analog_ping.ino
@@ -17,6 +17,17 @@
 	a = 100%
 	s = 0% - OFF
 
+ RobotMonitor HTTP CALLS
+	send command example:
+		LAN: http://192.168.1.180:50180/a
+		WAN: http://yourDomain:50180/a
+	send pingonline status example:
+		http://portal.robotmonitor.com/device-acces/write/?hash=yourDeviceAuthHashKey&data=pingonline
+	save DataTable example:
+		http://portal.robotmonitor.com/device-acces/write/?hash=yourDataTableINLink&data={"example":123}
+	read DataTable example:
+		http://portal.robotmonitor.com/device-acces/write/?hash=yourDataTableOUTLink
+
  Circuit:
   - Arduino
   - Arduino Ethernet shield
