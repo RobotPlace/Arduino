305b0fa3314afeb0ae7d979b4ce85f1a73f24c17
robotmonitor <admin@robotmonitor.com>
2015-04-14 08:34:22 +0200

    Update RobotMonitor3.h

--- libraries/RobotMonitor3/RobotMonitor3.h
+++ libraries/RobotMonitor3/RobotMonitor3.h
@@ -1,1 +1,23 @@
+#ifndef _ROBOTMONITOR_H_
+#define _ROBOTMONITOR_H_
 
+#include <Ethernet.h>
+
+class RobotMonitor {
+private:
+  IPAddress server;
+  EthernetClient client;
+  String authHash;
+  unsigned long pingperiod;
+  unsigned long lastping;
+  boolean clientConection;
+  boolean clientAnswered;
+  
+public:
+  void ping();
+  
+  RobotMonitor(const String authHash);
+  ~RobotMonitor();
+};
+
+#endif // _ROBOTMONITOR_H_
