25c2d333dac36463538872f3831efef84add3a57
robotmonitor <admin@robotmonitor.com>
2015-04-14 08:36:29 +0200

    Update RobotMonitor3.h

--- libraries/RobotMonitor3/RobotMonitor3.h
+++ libraries/RobotMonitor3/RobotMonitor3.h
@@ -2,21 +2,36 @@
 #define _ROBOTMONITOR_H_
 
 #include <Ethernet.h>
+#include <EEPROM.h>
+//#include <TrueRandom.h> //randomly generated MAC address
 
 class RobotMonitor {
 private:
+  byte devicemac[6];
+  IPAddress deviceIP;
   IPAddress server;
-  EthernetClient client;
+  EthernetClient clientPing;
+  EthernetClient clientData;
+  EthernetClient clientgetData;
   String authHash;
+  String tableinLink;
+  String tableoutLink;
   unsigned long pingperiod;
+  unsigned long dataperiod;
+  unsigned long getdataperiod;
+  unsigned long lastgetdata;
   unsigned long lastping;
-  boolean clientConection;
-  boolean clientAnswered;
-  
+  unsigned long lastdata;
+  String getDataBuffer;
+
 public:
   void ping();
-  
-  RobotMonitor(const String authHash);
+  void setData(const String& data);
+  void getData();
+  void setIP();
+  void setMAC();
+
+  RobotMonitor(const String& authHash, const String& tableinLink, const String& tableoutLink);
   ~RobotMonitor();
 };
 
